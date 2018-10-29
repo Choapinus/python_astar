aa6360a10803b0ed7d4043d1c86654c5b8971c3c
Don Choppy <wxtem@hotmail.com>
2018-10-29 11:09:47 -0300

    se termina tarea. Al final del archivo main se deja una nota importante

--- graph.py
+++ graph.py
@@ -1,13 +1,12 @@
 import os
-import collections
-import math
+from math import sqrt
 
 class Node:
 	def __init__(self, _tuple):
 		self._id = _tuple[0]
 		self.x = _tuple[1]
 		self.y = _tuple[2]
-		self.costo = 0
+		self.costo = 1
 	
 	def get_coordsx(self):
 		return self.x
@@ -18,35 +17,29 @@ class Node:
 	def get_id(self):
 		return self._id
 
-
 	@staticmethod
-	def distanceToCity(node1 , node2 ):
+	def del_node(node, list_nodes):
+		for i in range(len(list_nodes)):
+			if node._id == list_nodes[i]._id:
+				del list_nodes[i]
+				return list_nodes
+	
+	@staticmethod
+	def distanceToCity(node1 , node2): # euclidean
 		x = abs(node1.get_coordsx() - node2.get_coordsx())
 		y = abs(node1.get_coordsy() - node2.get_coordsy())
-		return math.sqrt(pow(x,2) + pow(y,2))
+		return sqrt(pow(x,2) + pow(y,2))
 	
 	def __str__(self):
 		return 'id {}: ({}, {})'.format(self._id, self.x, self.y)
-
-class SimpleGraph:
-	def __init__():
-		edges = {}
 	
-	def neighbors(self, _id):
-		return self.edges[_id]
-
-class Queue:
-	def __init__(self):
-		self.elements = collections.deque()
-
-	def empty(self):
-		return len(self.elements) == 0
-
-	def push(self, x):
-		self.elements.append(x)
-
-	def pop(self):
-		return self.elements.popleft()
+	@staticmethod
+	def get_heuristic(nodes):
+		heuristica = 0.0
+		for i in range(1,len(nodes)):
+			heuristica = heuristica + Node.distanceToCity(nodes[i-1],nodes[i])
+		# print("la heuristica es ->", heuristica)
+		return heuristica
 
 def get_nodes(txt='./nodes.txt'):
 	if os.path.isfile(txt):
@@ -56,3 +49,30 @@ def get_nodes(txt='./nodes.txt'):
 		return [Node(t) for t in nodes]
 	else:
 		raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), txt)
+
+def get_best_F(close_list, open_list):
+	g_x = 0.0
+	h_x = float("inf")
+	min_node = open_list[0]
+	
+	# comprobar que el nodo rescatado no esta en la close_list
+	for idx, node in enumerate(close_list):
+		if node.get_id() == min_node.get_id():
+			min_node = open_list[idx]
+	
+	# obtener el g_x del camino recorrido hasta ahora
+	if len(close_list) in [0, 1]:
+		g_x = 0
+	else:
+		for i in range(1, len(close_list)):
+			g_x += Node.distanceToCity(close_list[i-1], close_list[i])
+
+	# obtener el nodo de menor distancia (h_x)
+	if len(close_list) != 0:
+		for node in open_list:
+			hh = Node.distanceToCity(close_list[-1], node)
+			if hh < h_x:
+				h_x = hh
+				min_node = node
+	
+	return min_node
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-from graph import get_nodes, SimpleGraph
-from solver import get_heuristic, tsp_solver
+from graph import Node, get_nodes, get_best_F
 
-if __name__ == '__main__':
-	nodes = get_nodes()
 
-get_heuristic(nodes)
+import time
 
-tsp_solver(nodes)
+start = time.time()
 
-#print (nodes)
-"""
-for i in nodes:
-	print("id: ", i.get_id())
-	print("x: ", i.get_coordsx())
-	print("y: ", i.get_coordsy())	
-"""
+nodes = get_nodes()
+
+meta = nodes[-1]
+init_node = nodes[0]
+solution_found = False
+
+# print('init_node', init_node)
+# print('meta', meta, '\n')
+
+open_list = [nodes[0]]
+close_list = []
+nodes = Node.del_node(nodes[0], nodes)
+
+for node in nodes:
+    open_list.append(node)
+
+start_g = 0.0
+start_f = start_g + Node.distanceToCity(init_node, meta) # g+h
+
+while open_list != []:
+    best_node = get_best_F(close_list, open_list) # get node with best f(x)
+    
+    if best_node.get_id() == meta.get_id():
+        solution_found = True
+
+    close_list.append(best_node)
+    open_list = Node.del_node(best_node, open_list)
+
+close_list.append(meta)
+
+total_dist = 0.0
+
+for idx in range(1, len(close_list)):
+    total_dist += Node.distanceToCity(close_list[idx-1], close_list[idx])
+
+
+end = time.time()
+
+print('tiempo total:', end - start, 'segundos')
+print('distancia total:', total_dist)
+print('cantidad de expansiones:', len(close_list))
+
+# nota:
+# dado a que se esta trabajando con un grafo completo,
+# la expansion de todo nodo resultara en todos los demas nodos,
+# bajo esta logica, se agregan todos los nodos a la lista open una sola vez
